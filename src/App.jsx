8827f319b89afb433abd6d99544bc70860fae668
NonoIceOff <nonocraft28@gmail.com>
2025-01-22 15:47:27 +0100

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from "react";
 import "./App.css";
 import Register from "./view/auth/register.jsx";
 import Login from "./view/auth/login.jsx";
 import Home from "./view/home.jsx";
 import { BrowserRouter, Routes, Route } from "react-router";
+import './index.css'
+
+import Dashboard from './view/dashboard.jsx'
+import ViewBankAccounts from './view/viewBankAccounts.jsx'
+import ViewBankAccount from './view/viewBankAccount.jsx'
 
 
 function App() {
@@ -16,6 +20,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} /> 
           <Route path="*" element={<h1>La Page n'existe pas sale kiwi</h1>} />
+          <Route path="/bank_accounts" element={<ViewBankAccounts />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/account/:id/transactions" element={<ViewBankAccount />}/>
         </Routes>
       ) : (
         <Routes>
